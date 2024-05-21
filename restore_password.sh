9426e986a778b96b4c98ebfcf4b940b74697763b
Prince Tawiah <91223486+Prince1nOnly@users.noreply.github.com>
2024-05-21 23:59:53 +0000

    Update restore_password.sh

--- restore_password.sh
+++ restore_password.sh
@@ -2,5 +2,5 @@
 
 # Restore the password file from the persistent storage
 if [ -f /teamspace/studios/this_studio/shadow ]; then
-  sudo cp /teamspace/studios/this_studio/shadow /etc/shadow
+  sudo cp /teamspace/studios/this_studio/.config/user_creds/shadow /etc/shadow
 fi
