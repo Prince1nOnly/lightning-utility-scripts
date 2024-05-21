d5cbbebf5bc240fed9f5f6e415630ecb752366e0
Prince Tawiah <91223486+Prince1nOnly@users.noreply.github.com>
2024-05-21 23:57:21 +0000

    Update implement.sh

--- implement.sh
+++ implement.sh
@@ -1,26 +1,26 @@
 # Source the linuxbrew setup script
 if [ -f /teamspace/studios/this_studio/setup_linuxbrew.sh ]; then
-    source /teamspace/studios/this_studio/setup_linuxbrew.sh
+    source /teamspace/studios/this_studio/lightning-utility-scripts/setup_linuxbrew.sh
 fi
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Ensure password
 # Restore password state on shell startup
-if [ -f /teamspace/studios/this_studio/restore_password.sh ]; then
-  /teamspace/studios/this_studio/restore_password.sh
+if [ -f /teamspace/studios/this_studio/lightning-utility-scripts/restore_password.sh ]; then
+  /teamspace/studios/this_studio/lightning-utility-scripts/restore_password.sh
 fi
 
 # Function to handle shell exit
 function on_exit {
   # Call the script to save password
-  if [ -f /teamspace/studios/this_studio/save_password.sh ]; then
-    /teamspace/studios/this_studio/save_password.sh
+  if [ -f /teamspace/studios/this_studio/lightning-utility-scripts/save_password.sh ]; then
+    /teamspace/studios/this_studio/lightning-utility-scripts/save_password.sh
   fi
 
   # Call the script to move Jupyter kernels to the target directory
-  if [ -f /path/to/move_kernels.sh ]; then
-    /path/to/move_kernels.sh
+  if [ -f /teamspace/studios/this_studio/lightning-utility-scripts/move_kernels.sh ]; then
+    /teamspace/studios/this_studio/lightning-utility-scripts/move_kernels.sh
   fi
 }
 
