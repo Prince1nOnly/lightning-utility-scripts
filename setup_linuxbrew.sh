1cd414f2c12071096b6685803e62953d7dcafefd
Prince Tawiah <91223486+Prince1nOnly@users.noreply.github.com>
2024-05-22 14:24:12 +0000

    Update setup_linuxbrew.sh

--- setup_linuxbrew.sh
+++ setup_linuxbrew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define persistent storage directory
-PERSISTENT_LINUXBREW_DIR="/teamspace/studios/this_studio/.packages/linuxbrew"
+PERSISTENT_LINUXBREW_DIR="/teamspace/studios/this_studio/.packages/"
 
 # Ensure persistent storage directory exists
 mkdir -p "$PERSISTENT_LINUXBREW_DIR"
@@ -10,11 +10,8 @@ mkdir -p "$PERSISTENT_LINUXBREW_DIR"
 if [ ! -L "/home/linuxbrew" ]; then
     # If /home/linuxbrew exists and is a directory, move its contents
     if [ -d "/home/linuxbrew" ]; then
-        # Loop through files in /home/linuxbrew and move them individually
-        for file in /home/linuxbrew/*; do
-            sudo mv "$file" "$PERSISTENT_LINUXBREW_DIR"
-        done
+        sudo mv /home/linuxbrew/ "$PERSISTENT_LINUXBREW_DIR"/
     fi
     # Create the symbolic link
-    sudo ln -s "$PERSISTENT_LINUXBREW_DIR" /home/linuxbrew
+    sudo ln -s "$PERSISTENT_LINUXBREW_DIR" /home/
 fi
