58dde9634870449b32ef5b69ef65c93ee2c1d152
Prince Tawiah <91223486+Prince1nOnly@users.noreply.github.com>
2024-05-22 11:44:43 +0000

    Update configure_server.sh

--- configure_server.sh
+++ configure_server.sh
@@ -48,7 +48,7 @@ get_yes_no_response() {
     return 0
   fi
   while true; do
-    read -p "$1 (yes/no): " response
+    read -p "$1 (yes[Y/y] or no[N/n]): " response
     case "$response" in
       [yY][eE][sS]|[yY]) return 0 ;;
       [nN][oO]|[nN]) return 1 ;;
@@ -88,7 +88,7 @@ echo "8. Install essential packages."
 
 # Wait for the user to be ready if not using -y
 if ! $YES_TO_ALL; then
-  read -p "\n${YELLOW}Press Enter to start the configuration process...${NC}"
+  read -p $'\n'"${YELLOW}Press Enter to start the configuration process...${NC}"
 fi
 
 # Change user password
@@ -114,7 +114,7 @@ if get_yes_no_response "${YELLOW}Do you want to install Linuxbrew?${NC}"; then
   echo -e "${GREEN}Linuxbrew installed successfully.${NC}"
   if [ -f ./setup_linuxbrew.sh ]; then
     echo -e "\n${GREEN}Persisting Linuxbrew to storage...${NC}"
-    ./setup_linuxbrew.sh
+    source ./setup_linuxbrew.sh
     check_status "Linuxbrew setup"
     echo -e "${GREEN}Linuxbrew setup completed successfully.${NC}"
   else
