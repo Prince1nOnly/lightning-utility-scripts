a17bf1f5dc2c54912d9d64e3ccbcb3116d02a61e
Prince Tawiah <91223486+Prince1nOnly@users.noreply.github.com>
2024-05-22 00:00:34 +0000

    Update save_password.sh

--- save_password.sh
+++ save_password.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Save the password file to the persistent storage
-sudo cp /etc/shadow /teamspace/studios/this_studio/shadow
+sudo cp /etc/shadow /teamspace/studios/this_studio/.config/user_creds/shadow
